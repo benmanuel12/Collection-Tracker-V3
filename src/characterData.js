9372b410888a8c982c49adf597b68630bdc0f382
Ben Manuel <contact.ben.manuel@gmail.com>
2024-01-29 11:30:58 +0000

    Replaced character names with generic names

---
+++ src/characterData.js
@@ -0,0 +1,361 @@
+// This file originally contained names of video game characters.
+// I've replaced them with generic names generated from https://www.name-generator.org.uk/quick/
+// to prevent copyright issues.
+
+export default {
+    "success": true,
+    "data": {
+        "characters": [
+            {
+                id: "1",
+                name: "Eliot Cotton",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "2",
+                name: "Josie Lucero",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "3",
+                name: "Kieran Best",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "4",
+                name: "Seth Frederick",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "5",
+                name: "Eshal Vincent",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "6",
+                name: "Mckenzie Griffith",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "7",
+                name: "Gwen Cunningham",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "8",
+                name: "Claudia Mccarty",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "9",
+                name: "Teresa Bullock",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "10",
+                name: "Francis Winters",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "11",
+                name: "Arun Rollins",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "12",
+                name: "Keeley Andrade",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "13",
+                name: "Marley Bridges",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "14",
+                name: "Khalid Fitzpatrick",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "15",
+                name: "Frederick O'Brien",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "16",
+                name: "Ashley Orozco",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "17",
+                name: "Leonardo Hudson",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "18",
+                name: "Billie Erickson",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "19",
+                name: "Micheal Castaneda",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "20",
+                name: "Lily-Rose Torres",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "21",
+                name: "Sachin Herman",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "22",
+                name: "Oisin Richmond",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "23",
+                name: "Jordanne Kelley",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "24",
+                name: "Marc Foley",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "25",
+                name: "Oliwier Morrison",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "26",
+                name: "Celia Villanueva",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "27",
+                name: "Kara Wolf",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "28",
+                name: "Neo Alexander",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "29",
+                name: "Juan Moore",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "30",
+                name: "Maddison Holloway",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "31",
+                name: "Anya Hamilton",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "32",
+                name: "Rafferty Smith",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "33",
+                name: "Eleri Hicks",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "34",
+                name: "Shaun Estes",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "35",
+                name: "Tanisha Burke",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "36",
+                name: "Chelsey Bentley",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "37",
+                name: "Aran White",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "38",
+                name: "Aleesha Lopez",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "39",
+                name: "Doris Sherman",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "40",
+                name: "Cohen Li",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "41",
+                name: "Neha Moyer",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "42",
+                name: "Simeon Kim",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "43",
+                name: "Eddie Martinez",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "44",
+                name: "Haaris Krueger",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "45",
+                name: "Lisa Gordon",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "46",
+                name: "Sion Norton",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "47",
+                name: "Leah Kline",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "48",
+                name: "Douglas Atkins",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "49",
+                name: "Philippa Lester",
+                element: "none",
+                obtained: false
+            },
+
+            {
+                id: "50",
+                name: "Yash Cohen",
+                element: "none",
+                obtained: false
+            },
+
+        ]
+    }
+}
--- src/dataTranscriber.py
+++ src/dataTranscriber.py
@@ -1,3 +1,7 @@
+# Randomly using Python to convert a text file to a string that can be a JS object
+# There is no data.txt file, I removed it as I don't have the rights to the characters I'm listing
+# I'll also replace the names in characterData.js with generic names
+
 file = open("data.txt", "r")
 lines = file.readlines()
 file.close()
